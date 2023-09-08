8ebea5c0a6f6b3eed12e8e001c37b2821ea718c9
Roland Bewick <roland.bewick@gmail.com>
2023-09-08 16:45:11 +0700

    feat: send transactions, handle all networks and multiple uxos

--- src/App.tsx
+++ src/App.tsx
@@ -17,43 +17,78 @@ declare global {
     webbtc?: {
       enable(): Promise<void>;
       getAddress(): Promise<BitcoinAddress>;
-      signPsbt(psbt: string): Promise<string>;
+      signPsbt(psbt: string): Promise<{ signed: string }>;
+      on(event: "accountChanged", callback: () => void): void;
     };
   }
 }
-
-// TODO: load after address so network can be configured
-const {
-  bitcoin: { addresses, transactions },
-} = mempoolJS({
-  hostname: "mempool.space",
-  network: "testnet",
-});
 
 function App() {
   const [toAddress, setToAddress] = React.useState<string>(
     "tb1pmgqzlvj3kcnsaxvnvnjrfm2kyx2k9ddfp84ty6hx0972gz85gg3slq3j59"
   );
   const [toAmount, setToAmount] = React.useState<string>("100");
+  const [toFee, setToFee] = React.useState<string>("1000");
   const [address, setAddress] = React.useState<BitcoinAddress | undefined>();
   const [utxos, setUtxos] = React.useState<AddressTxsUtxo[] | undefined>();
+  const [isLoading, setLoading] = React.useState(true);
+  const [network, setNetwork] = React.useState<
+    "bitcoin" | "testnet" | undefined
+  >(undefined);
+  const [error, setError] = React.useState("");
+  const [sentTransactionId, setSentTransactionId] = React.useState("");
+
+  const load = React.useCallback(async () => {
+    setError("");
+    if (!window.webbtc) {
+      setError(
+        "window.webbtc does not exist. Please install the Alby extension"
+      );
+      return;
+    }
+    await window.webbtc.enable();
+
+    const address = await window.webbtc.getAddress();
+    setAddress(address);
+
+    const network = address.address.startsWith("tb")
+      ? "testnet"
+      : address.address.startsWith("bcrt")
+      ? "regtest"
+      : "bitcoin";
+    if (network === "regtest") {
+      setError(
+        "Sorry, Regtest not supported. Please change the network in your account."
+      );
+      return;
+    }
+    setNetwork(network);
+    const {
+      bitcoin: { addresses },
+    } = mempoolJS({
+      hostname: "mempool.space",
+      network,
+    });
+
+    const addressTxsUtxos = await addresses.getAddressTxsUtxo({
+      address: address.address,
+    });
+    console.log(addressTxsUtxos);
+    setUtxos(addressTxsUtxos);
+    setLoading(false);
+  }, []);
 
   React.useEffect(() => {
     (async () => {
-      if (!window.webbtc) {
-        return;
+      await load();
+      if (window.webbtc) {
+        window.webbtc.on("accountChanged", () => {
+          setLoading(true);
+          load();
+        });
       }
-      await window.webbtc.enable();
-
-      const address = await window.webbtc.getAddress();
-      setAddress(address);
-      const addressTxsUtxos = await addresses.getAddressTxsUtxo({
-        address: address.address,
-      });
-      console.log(addressTxsUtxos);
-      setUtxos(addressTxsUtxos);
     })();
-  }, []);
+  }, [load]);
 
   const balance =
     utxos?.map((utxo) => utxo.value).reduce((a, b) => a + b, 0) || 0;
@@ -66,12 +101,19 @@ function App() {
     if (!address) {
       throw new Error("Address not loaded");
     }
+    if (!network) {
+      throw new Error("Network not loaded");
+    }
     if (!utxos?.length) {
       throw new Error("UTXOs not loaded or no UTXOs");
+    }
+    if (!toAmount) {
+      throw new Error("Please send at least 1 sat");
     }
     console.log(
       toAddress,
       toAmount,
+      toFee,
       address.publicKey,
       address.publicKey.length
     );
@@ -81,6 +123,7 @@ function App() {
     bitcoin.initEccLib(ecc);
     const { output } = bitcoin.payments.p2tr({
       internalPubkey: xOnlyPubkey,
+      network: bitcoin.networks[network],
     });
 
     if (!output) {
@@ -88,51 +131,82 @@ function App() {
     }
 
     const amount = parseInt(toAmount);
-    // FIXME: support multiple utxos
-    const hash = utxos[0].txid;
-    const index = utxos[0].vout;
+    const fee = parseInt(toFee); // TODO: fee should be calculated
+    const total = amount + fee;
 
-    if (amount > utxos[0].value) {
-      throw new Error(
-        "amount " + amount + " is greater than utxo[0] " + utxos[0].value
-      );
+    const usedUtxos: AddressTxsUtxo[] = [];
+    let usedUtxoBalance = 0;
+
+    for (let i = 0; i < utxos.length; i++) {
+      usedUtxos.push(utxos[i]);
+      usedUtxoBalance += utxos[i].value;
+      if (usedUtxoBalance >= total) {
+        break;
+      }
     }
 
-    const psbt = new bitcoin.Psbt({ network: bitcoin.networks.testnet })
-      .addInput({
-        hash,
-        index,
-        witnessUtxo: { value: utxos[0].value, script: output },
+    if (usedUtxoBalance < total) {
+      throw new Error("not enough balance");
+    }
+
+    const psbt = new bitcoin.Psbt({ network: bitcoin.networks[network] });
+    for (const utxo of usedUtxos) {
+      psbt.addInput({
+        hash: utxo.txid,
+        index: utxo.vout,
+        witnessUtxo: { value: utxo.value, script: output },
         tapInternalKey: xOnlyPubkey,
-      })
-      // FIXME: add a change address output
-      .addOutput({
-        value: amount,
+      });
+    }
 
-        address: toAddress,
-      })
-      .toHex();
-
-    console.log("PSBT hex:", psbt);
-
-    const signed = await window.webbtc.signPsbt(psbt);
-    console.log("SIGNED:", signed);
-
-    const result = await transactions.postTx({
-      txhex: signed,
+    psbt.addOutput({
+      value: amount,
+      address: toAddress,
     });
 
-    console.log(result);
+    const remainder = usedUtxoBalance - total;
 
-    //const signedTransaction = bitcoin.Transaction.fromHex(signed);
+    if (remainder > 0) {
+      // send remainder back to original address
+      psbt.addOutput({
+        value: remainder,
+        address: address.address,
+      });
+    }
 
-    // await regtestUtils.broadcast(tx.toHex());
-    // await regtestUtils.verify({
-    //   txId: tx.getId(),
-    //   address: regtestUtils.RANDOM_ADDRESS,
-    //   vout: 0,
-    //   value: sendAmount,
-    // });
+    const psbtHex = psbt.toHex();
+    console.log("PSBT hex:", psbtHex);
+
+    const signResponse = await window.webbtc.signPsbt(psbtHex);
+    console.log("SIGNED:", signResponse);
+
+    const result = await fetch(
+      `https://blockstream.info/${
+        network === "testnet" ? "testnet/" : ""
+      }api/tx`,
+      {
+        method: "POST",
+        body: signResponse.signed,
+
+        headers: new Headers({
+          "Content-Type": "text/plain",
+        }),
+      }
+    );
+
+    if (result.ok) {
+      const sentTransactionId = await result.text();
+      setSentTransactionId(sentTransactionId);
+      //alert("Transaction broadcasted: " + sentTransactionId);
+    } else {
+      const error = await result.text();
+      console.error(result.status, error);
+      setError(error);
+    }
+  }
+
+  if (error) {
+    return error;
   }
 
   return (
@@ -142,48 +216,72 @@ function App() {
         <p>
           {!window.webbtc
             ? "No webbtc extension detected. Try Alby"
-            : address
-            ? address?.address
-            : "Connecting..."}
+            : isLoading || !address
+            ? "Connecting..."
+            : address.address}
         </p>
+        {network && <p>Network: {network}</p>}
       </div>
-      <p>Balance</p>
-      <p>{utxos ? `${balance} sats` : "Loading..."}</p>
-      {utxos && (
+      {!isLoading && (
         <>
+          <p>Balance</p>
+          <p>{utxos ? `${balance} sats` : "Loading..."}</p>
           {utxos && (
-            <div>
-              <p>UTXOs</p>
-              {utxos.map((utxo) => (
-                <p key={utxo.txid}>
-                  {utxo.txid} {utxo.value}
+            <>
+              {utxos && (
+                <div>
+                  <p>UTXOs</p>
+                  {utxos.map((utxo) => (
+                    <p key={utxo.txid}>
+                      {utxo.txid} {utxo.value}
+                    </p>
+                  ))}
+                </div>
+              )}
+              {balance ? (
+                <div>
+                  <p>Send sats</p>
+                  <form onSubmit={sendSats}>
+                    Address{" "}
+                    <input
+                      onChange={(e) => setToAddress(e.target.value)}
+                      placeholder="tb1..."
+                      defaultValue={toAddress}
+                    />
+                    Amount{" "}
+                    <input
+                      onChange={(e) => setToAmount(e.target.value)}
+                      placeholder="amount in sats"
+                      defaultValue={toAmount}
+                    />
+                    Total fee{" "}
+                    <input
+                      onChange={(e) => setToFee(e.target.value)}
+                      placeholder="amount in sats"
+                      defaultValue={toAmount}
+                    />
+                    <button>Submit</button>
+                  </form>
+                </div>
+              ) : (
+                <p>
+                  You don't have any UTXOs. Send some sats to your address using
+                  a testnet faucet.
                 </p>
-              ))}
-            </div>
+              )}
+            </>
           )}
-          {balance ? (
-            <div>
-              <p>Send sats</p>
-              <form onSubmit={sendSats}>
-                Address{" "}
-                <input
-                  onChange={(e) => setToAddress(e.target.value)}
-                  placeholder="tb1..."
-                  defaultValue={toAddress}
-                />
-                Amount{" "}
-                <input
-                  onChange={(e) => setToAmount(e.target.value)}
-                  placeholder="amount in sats"
-                  defaultValue={toAmount}
-                />
-                <button>Submit</button>
-              </form>
-            </div>
-          ) : (
+
+          {sentTransactionId && (
             <p>
-              You don't have any UTXOs. Send some sats to your address using a
-              testnet faucet.
+              Sent transaction!{" "}
+              <a
+                className="text-blue-500 underline"
+                href={`https://mempool.space/testnet/tx/${sentTransactionId}`}
+                target="_blank"
+              >
+                {sentTransactionId}
+              </a>
             </p>
           )}
         </>
