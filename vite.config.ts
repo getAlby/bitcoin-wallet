e66e65be73dbaab48a5db7ae6ffaed613ae84b4e
Roland Bewick <roland.bewick@gmail.com>
2023-09-10 15:59:08 +0700

    fix: vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills({
-    //include: ['buffer'],
-    // Whether to polyfill specific globals.
-    globals: {
-      Buffer: true, // can also be 'build', 'dev', or false
-    },
-    protocolImports: true,
-  }),],
-})
+  plugins: [
+    react(),
+    nodePolyfills({
+      //include: ['buffer'],
+      // Whether to polyfill specific globals.
+      globals: {
+        Buffer: true, // can also be 'build', 'dev', or false
+      },
+      protocolImports: true,
+    }),
+  ],
+  base: "/bitcoin-wallet/",
+});
